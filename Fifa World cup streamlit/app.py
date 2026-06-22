e514fb7fb8a1a538a3087c94e353fcfc8e9134ed
Qurat ul ain <quratulain.sera@gmail.com>
2026-06-22 18:58:06 +0500

    Add World Cup Streamlit project folder

--- Fifa World cup streamlit/app.py
+++ Fifa World cup streamlit/app.py
@@ -8,9 +8,23 @@ st.title('World Cup Matches')
 # load the file 
 df = pd.read_csv('WorldCupMatches.csv')
 st.write(df)
-df.shape
+
+# Data cleaning
+df = df.fillna(0)
+df = df.drop_duplicates()
+df.reset_index(drop=True, inplace=True)
+
+
+df['Year'] = df['Year'].astype(int)
+df['Attendance'] = df['Attendance'].astype(int)
+
 
 # A side bar
 st.sidebar.title('Select Year')
 years = df['Year'].unique()
-st.sidebar._selectbox('label', options=years)
+st.sidebar.selectbox('label', options=years)
+
+total_matches = len(df)
+total_goals = sum(df['Home Team Goals']) + sum(['Away Team Goals'])
+total_tournaments = sum(df['Year'].unique())
+st.columns(4)
